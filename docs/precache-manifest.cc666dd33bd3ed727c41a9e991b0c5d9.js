12e345e83aa2c718d0a12a8e2ea51f292f31ac7e
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2019-02-11 15:11:09 +0100

    change by "./"

---
+++ docs/precache-manifest.cc666dd33bd3ed727c41a9e991b0c5d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "2b1e7704c1b3546d7727",
+    "url": "js/chunk-vendors.847ece9f.js"
+  },
+  {
+    "revision": "3605f5afeb7a6036aa87",
+    "url": "js/app.ebe9e3b1.js"
+  },
+  {
+    "revision": "366f2728cf9b5ef80d4c",
+    "url": "js/about.b9378e84.js"
+  },
+  {
+    "revision": "53765657953bc87f586ceb352b369a26",
+    "url": "index.html"
+  },
+  {
+    "revision": "c7f2de5c110703b85707ea0d12b84a43",
+    "url": "img/m7.png"
+  },
+  {
+    "revision": "3812354b7eaf58d6056a5c9b2f712247",
+    "url": "img/m5.png"
+  },
+  {
+    "revision": "b6ec2ce8847884e4000ce971188ec0f4",
+    "url": "img/m11.png"
+  },
+  {
+    "revision": "2b1e7704c1b3546d7727",
+    "url": "css/chunk-vendors.5dd072d7.css"
+  },
+  {
+    "revision": "3605f5afeb7a6036aa87",
+    "url": "css/app.4fa4d9b3.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/musaicbox/precache-manifest.9bd4c4e583ef765bc451d5588aa8e0f7.js"
+  "precache-manifest.cc666dd33bd3ed727c41a9e991b0c5d9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "musaicbox"});
