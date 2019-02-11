1b311470aa333eb79d57f4014f658f3ccea04a63
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2019-02-11 11:55:34 +0100

    add github pages docs folder (vue-gh-pages npm module)

---
+++ docs/precache-manifest.766dcf04ff85a64c0003f8f3ab5d9abf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2b1e7704c1b3546d7727",
+    "url": "/js/chunk-vendors.847ece9f.js"
+  },
+  {
+    "revision": "35231321b9747cdbfabd",
+    "url": "/js/app.b0999ad7.js"
+  },
+  {
+    "revision": "7a4a744f1b4e8c356c94",
+    "url": "/js/about.d133a650.js"
+  },
+  {
+    "revision": "5b3ea6111578c0ddcd953c7244273366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ec2ce8847884e4000ce971188ec0f4",
+    "url": "/img/m11.b6ec2ce8.png"
+  },
+  {
+    "revision": "2b1e7704c1b3546d7727",
+    "url": "/css/chunk-vendors.5dd072d7.css"
+  },
+  {
+    "revision": "35231321b9747cdbfabd",
+    "url": "/css/app.cb5f378b.css"
+  }
+];
