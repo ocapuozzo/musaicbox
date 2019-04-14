71c2181773f768ccd1dffffd2e49e86dd8dfbac1
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2019-04-14 15:23:58 +0200

    build commit (precache manifest)

---
+++ docs/precache-manifest.515d4d28837459a7017e4c056a398318.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "21627de1ea2a78e5317d",
+    "url": "/musaicbox/js/about.be699205.js"
+  },
+  {
+    "revision": "97db4581754d97aab072",
+    "url": "/musaicbox/css/app.d27f1367.css"
+  },
+  {
+    "revision": "97db4581754d97aab072",
+    "url": "/musaicbox/js/app.ef0d62b6.js"
+  },
+  {
+    "revision": "f392fd739e7eb6611371",
+    "url": "/musaicbox/css/chunk-vendors.6acb9ff2.css"
+  },
+  {
+    "revision": "f392fd739e7eb6611371",
+    "url": "/musaicbox/js/chunk-vendors.72c892c2.js"
+  },
+  {
+    "revision": "f23c3b653e94bf7cb2888410a97ef43d",
+    "url": "/musaicbox/index.html"
+  },
+  {
+    "revision": "139b7ad41f580e330da051fc2a3ce0b2",
+    "url": "/musaicbox/favicon.png"
+  },
+  {
+    "revision": "7e826a81c500d1c8114838168d080de5",
+    "url": "/musaicbox/img/cplt.png"
+  },
+  {
+    "revision": "b6ec2ce8847884e4000ce971188ec0f4",
+    "url": "/musaicbox/img/m11.png"
+  },
+  {
+    "revision": "3812354b7eaf58d6056a5c9b2f712247",
+    "url": "/musaicbox/img/m5.png"
+  },
+  {
+    "revision": "c7f2de5c110703b85707ea0d12b84a43",
+    "url": "/musaicbox/img/m7.png"
+  },
+  {
+    "revision": "4d9b812677c7120f635b89a88f158e4e",
+    "url": "/musaicbox/img/t-.png"
+  },
+  {
+    "revision": "da64f03504b1e2d3e108e62a7e9f7794",
+    "url": "/musaicbox/img/t+.png"
+  },
+  {
+    "revision": "d40a1aac7d940696332fbfe242951c63",
+    "url": "/musaicbox/img/twotone-rotate_left-24px.png"
+  },
+  {
+    "revision": "be4c2865cd0cbd1f62e28d06586bdaeb",
+    "url": "/musaicbox/img/twotone-rotate_left-24px.svg"
+  },
+  {
+    "revision": "e8309ca65e41dfc1262f4923475319a4",
+    "url": "/musaicbox/img/twotone-rotate_right-24px.png"
+  },
+  {
+    "revision": "afee14e5c9c10c200717fd22707b0db5",
+    "url": "/musaicbox/img/twotone-rotate_right-24px.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/musaicbox/robots.txt"
+  }
+];
