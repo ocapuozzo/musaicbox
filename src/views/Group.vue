88a67894d20a005b9d54ac9d8f391528cf96c18c
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2019-05-18 10:44:48 +0200

    add pid (polynimial function id) corr bug pf musaic

---
+++ src/views/Group.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is a tool support of 88musaics.org </h1>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      TODO ...
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true"><font-awesome-icon icon="yin-yang"  size="lg"/>  </span>
+      </button>
+    </div>
+  </div>
+</template>
