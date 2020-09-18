957dd7d939ca8722165a19f0c88816dcc0ce646f
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2020-09-18 08:45:26 +0200

    npm update

---
+++ docs/precache-manifest.ba9858d29b68a3435aebcdc3b2ccf7a2.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "be008fd06f17c1ab3dc9",
+    "url": "/musaicbox/css/about.e1ffe27b.css"
+  },
+  {
+    "revision": "be008fd06f17c1ab3dc9",
+    "url": "/musaicbox/js/about.37f5a4d4.js"
+  },
+  {
+    "revision": "be008fd06f17c1ab3dc9",
+    "url": "/musaicbox/css/about.e1ffe27b.css.map"
+  },
+  {
+    "revision": "be008fd06f17c1ab3dc9",
+    "url": "/musaicbox/js/about.37f5a4d4.js.map"
+  },
+  {
+    "revision": "488bb49ae7288d540448",
+    "url": "/musaicbox/css/app.c0ac2f90.css"
+  },
+  {
+    "revision": "488bb49ae7288d540448",
+    "url": "/musaicbox/js/app.4c82424e.js"
+  },
+  {
+    "revision": "488bb49ae7288d540448",
+    "url": "/musaicbox/css/app.c0ac2f90.css.map"
+  },
+  {
+    "revision": "488bb49ae7288d540448",
+    "url": "/musaicbox/js/app.4c82424e.js.map"
+  },
+  {
+    "revision": "147a0c67b3f2e06c3d9e",
+    "url": "/musaicbox/css/chunk-vendors.d8c0d0c1.css"
+  },
+  {
+    "revision": "147a0c67b3f2e06c3d9e",
+    "url": "/musaicbox/js/chunk-vendors.4522891a.js"
+  },
+  {
+    "revision": "147a0c67b3f2e06c3d9e",
+    "url": "/musaicbox/css/chunk-vendors.d8c0d0c1.css.map"
+  },
+  {
+    "revision": "147a0c67b3f2e06c3d9e",
+    "url": "/musaicbox/js/chunk-vendors.4522891a.js.map"
+  },
+  {
+    "revision": "004789734626b9cc5929278fa0338b86",
+    "url": "/musaicbox/index.html"
+  },
+  {
+    "revision": "59eb417a302ee2c78fcd2f2af73b91cc",
+    "url": "/musaicbox/analyse/analyse.puml"
+  },
+  {
+    "revision": "11730d11204bfe457fece57132cb0963",
+    "url": "/musaicbox/analyse/button-show-limited-transposition.png"
+  },
+  {
+    "revision": "8ddde5bf4a4574bb53a1d8353f8c51e4",
+    "url": "/musaicbox/analyse/Limited-Transposition-IS.png"
+  },
+  {
+    "revision": "97892d8a21837400932d4db36f03c650",
+    "url": "/musaicbox/analyse/orbitsByISMotif.png"
+  },
+  {
+    "revision": "3631f7c2147a45d728b7b05d70b0030f",
+    "url": "/musaicbox/analyse/orbitsByStabilizers.png"
+  },
+  {
+    "revision": "9c6061bba07bea641fcecf1cbda90d65",
+    "url": "/musaicbox/analyse/setAnalyse.dia"
+  },
+  {
+    "revision": "55d47c764d2234988b50a4da84d5778d",
+    "url": "/musaicbox/analyse/setAnalyse.png"
+  },
+  {
+    "revision": "23dcb0893907e4061ef9b9fe35f8f34a",
+    "url": "/musaicbox/analyse/setAnalysePartionned.dia"
+  },
+  {
+    "revision": "f15c1cf65fc85c93ca84bbaf13f2fe9b",
+    "url": "/musaicbox/analyse/setAnalysePartionned.png"
+  },
+  {
+    "revision": "dbbbd3f7301bd024399ffe0cdc12e6e8",
+    "url": "/musaicbox/analyse/TL-IS-interface.png"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/musaicbox/favicon.ico"
+  },
+  {
+    "revision": "dcee68e2eb197cbcd3427439bc287c72",
+    "url": "/musaicbox/favicon.png"
+  },
+  {
+    "revision": "38d1f3451c1fd0c56e3613ff88efb58d",
+    "url": "/musaicbox/images/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "313a111abb52a672b5af4059d9875080",
+    "url": "/musaicbox/images/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "dd125a7ed8a053b409cf8a49c414d59e",
+    "url": "/musaicbox/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "2d100bc5f6372171fc680736ed541432",
+    "url": "/musaicbox/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "24efd85d023ecf3af566ba880ed8fc0b",
+    "url": "/musaicbox/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "dbe09f93f0ba4caf67dcde3d565c3e1a",
+    "url": "/musaicbox/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "905a2bae040f2ba9aaafe05fcadc00c5",
+    "url": "/musaicbox/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "8a72827bbb83702c4de79f3c90f24ed8",
+    "url": "/musaicbox/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "dcee68e2eb197cbcd3427439bc287c72",
+    "url": "/musaicbox/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "1a41247d6e5af46568a3391a5108b178",
+    "url": "/musaicbox/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "7e826a81c500d1c8114838168d080de5",
+    "url": "/musaicbox/img/cplt.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/musaicbox/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/musaicbox/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/musaicbox/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/musaicbox/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/musaicbox/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/musaicbox/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/musaicbox/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/musaicbox/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/musaicbox/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/musaicbox/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/musaicbox/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/musaicbox/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/musaicbox/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "84a322e887b1c6326371485f35c3dcc2",
+    "url": "/musaicbox/img/m11.png"
+  },
+  {
+    "revision": "3812354b7eaf58d6056a5c9b2f712247",
+    "url": "/musaicbox/img/m5.png"
+  },
+  {
+    "revision": "c7f2de5c110703b85707ea0d12b84a43",
+    "url": "/musaicbox/img/m7.png"
+  },
+  {
+    "revision": "4d9b812677c7120f635b89a88f158e4e",
+    "url": "/musaicbox/img/t-.png"
+  },
+  {
+    "revision": "da64f03504b1e2d3e108e62a7e9f7794",
+    "url": "/musaicbox/img/t+.png"
+  },
+  {
+    "revision": "d40a1aac7d940696332fbfe242951c63",
+    "url": "/musaicbox/img/twotone-rotate_left-24px.png"
+  },
+  {
+    "revision": "be4c2865cd0cbd1f62e28d06586bdaeb",
+    "url": "/musaicbox/img/twotone-rotate_left-24px.svg"
+  },
+  {
+    "revision": "e8309ca65e41dfc1262f4923475319a4",
+    "url": "/musaicbox/img/twotone-rotate_right-24px.png"
+  },
+  {
+    "revision": "afee14e5c9c10c200717fd22707b0db5",
+    "url": "/musaicbox/img/twotone-rotate_right-24px.svg"
+  },
+  {
+    "revision": "3191c7cfa04fcdb103163073a03acf34",
+    "url": "/musaicbox/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/musaicbox/robots.txt"
+  }
+];
